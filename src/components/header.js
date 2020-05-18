a102476fe6ff629f9aeaa012a783569577e3cbef
Austin <austin.blade@relode.com>
2020-05-17 20:54:55 -0500

    put nave scroll in useEffect hook

--- src/components/header.js
+++ src/components/header.js
@@ -5,58 +5,62 @@ import React from "react";
 
 import AustinBladeLogo from "../images/austinbladelogo";
 
-if (typeof window !== undefined) {
-  let prevScrollpos = window.pageYOffset
+const Header = ({ siteTitle }) => {
+  React.useEffect(() => {
+    let prevScrollpos = window.pageYOffset
 
-  window.onscroll = function () {
-    let currentScrollPos = window.pageYOffset
-    if (prevScrollpos > currentScrollPos) {
-      document.getElementById("navbar").style.top = "0"
-    } else {
-      document.getElementById("navbar").style.top = "-75px"
+    window.onscroll = function () {
+      let currentScrollPos = window.pageYOffset
+      if (prevScrollpos > currentScrollPos) {
+        document.getElementById("navbar").style.top = "0"
+      } else {
+        document.getElementById("navbar").style.top = "-75px"
+      }
+      prevScrollpos = currentScrollPos
     }
-    prevScrollpos = currentScrollPos
-  }
+  }, [])
+
+  return (
+    <header>
+      <div
+        id="navbar"
+        style={{
+          margin: `0 auto`,
+          width: `100%`,
+          padding: `1.0rem 1.0875rem`,
+          display: `flex`,
+          justifyContent: `space-between`,
+          backgroundColor: `var(--bg)`,
+          boxShadow: `var(--cardShadow)`,
+          position: `fixed`,
+          top: 0,
+          height: 75,
+          transition: `top 0.3s`,
+        }}
+      >
+        <Link to="/">
+          <AustinBladeLogo width="60px" />
+        </Link>
+
+        <ThemeToggler>
+          {({ theme, toggleTheme }) => (
+            <div className="container">
+              <label>
+                <input
+                  type="checkbox"
+                  onChange={e =>
+                    toggleTheme(e.target.checked ? "dark" : "light")
+                  }
+                  checked={theme === "dark"}
+                />{" "}
+              </label>
+            </div>
+          )}
+        </ThemeToggler>
+      </div>
+    </header>
+  )
 }
-
-const Header = ({ siteTitle }) => (
-  <header>
-    <div
-      id="navbar"
-      style={{
-        margin: `0 auto`,
-        width: `100%`,
-        padding: `1.0rem 1.0875rem`,
-        display: `flex`,
-        justifyContent: `space-between`,
-        backgroundColor: `var(--bg)`,
-        boxShadow: `var(--cardShadow)`,
-        position: `fixed`,
-        top: 0,
-        height: 75,
-        transition: `top 0.3s`,
-      }}
-    >
-      <Link to="/">
-        <AustinBladeLogo width="60px" />
-      </Link>
-
-      <ThemeToggler>
-        {({ theme, toggleTheme }) => (
-          <div className="container">
-            <label>
-              <input
-                type="checkbox"
-                onChange={e => toggleTheme(e.target.checked ? "dark" : "light")}
-                checked={theme === "dark"}
-              />{" "}
-            </label>
-          </div>
-        )}
-      </ThemeToggler>
-    </div>
-  </header>
-)
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
